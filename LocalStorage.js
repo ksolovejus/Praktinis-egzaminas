ea134bed1642537e94646efe4b55833ccb7aa679
ksolovejus <solovejuskarolis@gmail.com>
2023-05-07 23:54:39 +0300

    after localStorage

--- LocalStorage.js
+++ LocalStorage.js
@@ -3,10 +3,25 @@ var firstNameInput = document.querySelector('input[placeholder="First name"]');
 var lastNameInput = document.querySelector('input[placeholder="Last name"]');
 var phoneInput = document.querySelector('input[placeholder="Phone number"]');
 
-// Set the value of the last filled input fields
 firstNameInput.value = localStorage.getItem('firstName') || '';
 lastNameInput.value = localStorage.getItem('lastName') || '';
 phoneInput.value = localStorage.getItem('phoneNumber') || '';
+
+// Input radio for header
+var subscriptionElement = document.getElementsByName("subscription");
+var value = localStorage.getItem('subscription');
+
+for (var i=0; i<subscriptionElement.length; i++){
+    if(subscriptionElement[i].value == value){
+        subscriptionElement[i].checked = true;
+    }
+}
+
+document.querySelectorAll('input[name="subscription"]').forEach(function(element) {
+    element.addEventListener('change', function() {
+        localStorage.setItem('subscription', this.value);
+    });
+});
 
 
 // Input text and tel for section-5
@@ -14,12 +29,10 @@ var firstNameInput2 = document.querySelector('#FName2');
 var lastNameInput2 = document.querySelector('#LName2');
 var phoneInput2 = document.querySelector('#tel2');
 
-// Set the value of the last filled input fields
 firstNameInput2.value = localStorage.getItem('firstName2') || '';
 lastNameInput2.value = localStorage.getItem('lastName2') || '';
 phoneInput2.value = localStorage.getItem('phoneNumber2') || '';
 
-// Add event listeners to input fields
 firstNameInput2.addEventListener('input', function() {
   localStorage.setItem('firstName2', this.value);
 });
@@ -33,7 +46,6 @@ phoneInput2.addEventListener('input', function() {
 });
 
 
-// Add event listeners to input fields
 firstNameInput.addEventListener('input', function() {
   localStorage.setItem('firstName', this.value);
 });
@@ -45,6 +57,8 @@ lastNameInput.addEventListener('input', function() {
 phoneInput.addEventListener('input', function() {
   localStorage.setItem('phoneNumber', this.value);
 });
+
+
 // Input radio for section-5
 var personalRadio = document.getElementById('PERSONAL');
 var companyRadio = document.getElementById('COMPANY');
